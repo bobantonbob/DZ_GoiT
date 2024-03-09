6d1b4dc52107ba6f98a8745621d566e111723518
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-03-09 17:32:18 +0200

    v.0.0.2 )

--- DZ10/root/quotes/views.py
+++ DZ10/root/quotes/views.py
@@ -11,5 +11,3 @@ def main(request, page=1):
     paginator = Paginator(list(quotes), per_page)
     quotes_on_page = paginator.page(page)
     return render(request, 'quotes/index.html', context={'quotes': quotes_on_page})
-
-
