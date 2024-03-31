0631ce25700f89f6cf483c96fa4baed2602e855c
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-03-31 23:01:51 +0300

    dz12

---
+++ DZ12_Fast_Api_Users/src/entity/models.py
@@ -0,0 +1,42 @@
+import enum
+from datetime import date
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Integer, ForeignKey, DateTime, func, Enum
+from sqlalchemy.orm import DeclarativeBase
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID, generics
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Contact(Base):
+    __tablename__ = "contacts"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(20), index=True)
+    last_name: Mapped[str] = mapped_column(String(20), index=True)
+    email: Mapped[str] = mapped_column(String(40), index=True)
+    phone_number: Mapped[str] = mapped_column(String(20), index=True)
+    birthday: Mapped[str] = mapped_column(String(20), index=True)
+    extra_info: Mapped[str] = mapped_column(String(250))
+    completed: Mapped[bool] = mapped_column(default=False)
+    created_at: Mapped[date] = mapped_column('created_at', DateTime, default=func.now(), nullable=True)
+    updated_at: Mapped[date] = mapped_column('updated_at', DateTime, default=func.now(), onupdate=func.now(),
+                                             nullable=True)
+    user_id: Mapped[int] = mapped_column(generics.GUID(), ForeignKey('user.id'), nullable=True)
+    user: Mapped["User"] = relationship("User", backref="contacts", lazy="joined")
+
+
+class Role(enum.Enum):
+    admin: str = "admin"
+    moderator: str = "moderator"
+    user: str = "user"
+
+
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    username: Mapped[str] = mapped_column(String(50))
+    avatar: Mapped[str] = mapped_column(String(255), nullable=True)
+    created_at: Mapped[date] = mapped_column('created_at', DateTime, default=func.now())
+    updated_at: Mapped[date] = mapped_column('updated_at', DateTime, default=func.now(), onupdate=func.now())
+    role: Mapped[Enum] = mapped_column('role', Enum(Role), default=Role.user, nullable=True)
---
+++ DZ12_Fast_Api_Users/src/repository/contacts.py
@@ -0,0 +1,76 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
+
+from src.entity.models import Contact, User
+from src.schemas.contact import ContactSchema, ContactUpdateSchema
+
+
+async def get_contacts(limit: int, offset: int, db: AsyncSession, user: User):
+    stmt = select(Contact).filter_by(user=user).offset(offset).limit(limit)
+    contacts = await db.execute(stmt)
+    return contacts.scalars().all()
+
+async def get_all_contacts(limit: int, offset: int, db: AsyncSession):
+    stmt = select(Contact).offset(offset).limit(limit)
+    contacts = await db.execute(stmt)
+    return contacts.scalars().all()
+
+
+async def get_contact(contact_id: int, db: AsyncSession, user: User):
+    stmt = select(Contact).filter_by(id=contact_id, user=user)
+    contact = await db.execute(stmt)
+    return contact.scalar_one_or_none()
+
+
+async def create_contact(body: ContactSchema, db: AsyncSession, user: User):
+    contact = Contact(**body.model_dump(exclude_unset=True), user=user)
+    db.add(contact)
+    await db.commit()
+    await db.refresh(contact)
+    return contact
+
+
+async def update_contact(contact_id: int, body: ContactUpdateSchema, db: AsyncSession, user: User):
+    stmt = select(Contact).filter_by(id=contact_id, user=user)
+    result = await db.execute(stmt)
+    contact = result.scalar_one_or_none()
+    if contact:
+        contact.first_name = body.first_name
+        contact.last_name = body.last_name
+        contact.email = body.email
+        contact.phone_number = body.phone_number
+        contact.birthday = body.birthday
+        contact.extra_info = body.extra_info
+        await db.commit()
+        await db.refresh(contact)
+    return contact
+
+
+async def delete_contact(contact_id: int, db: AsyncSession, user: User):
+    stmt = select(Contact).filter_by(id=contact_id, user=user)
+    contact = await db.execute(stmt)
+    contact = contact.scalar_one_or_none()
+    if contact:
+        await db.delete(contact)
+        await db.commit()
+    return contact
+
+
+
+# async def search_contacts(param: dict, db: Session):
+#     query = db.query(Contact)
+#     first_name = param.get("first_name")
+#     # last_name = param.get("last_name")
+#     # email = param.get("email")
+#     if first_name:
+#         query = query.filter(Contact.first_name.ilike(f"%{first_name}%"))
+#     # if last_name:
+#     #     query = query.filter(Contact.last_name.ilike(f"%{last_name}%"))
+#     # if email:
+#     #     query = query.filter(Contact.email.ilike(f"%{email}%"))
+#     contacts = query.offset(param.get("skip")).limit(param.get("limit"))
+#     return contacts
+#
+
+
