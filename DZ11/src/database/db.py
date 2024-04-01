0a7d455b8efb539cffd96b4519d8a2167488339e
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-04-01 22:14:26 +0300

    dz12 bcrypt="3.2.2" виправлена помилка виводу помилок

--- DZ11/src/database/db.py
+++ DZ11/src/database/db.py
@@ -18,8 +18,7 @@ class DatabaseSessionManager:
         session = self._session_maker()
         try:
             yield session
-        except Exception as err:
-            print(err)
+        except:
             await session.rollback()
         finally:
             await session.close()
@@ -30,4 +29,4 @@ sessionmanager = DatabaseSessionManager(config.DB_URL)
 
 async def get_db():
     async with sessionmanager.session() as session:
-        yield session
+        return session
