a2a6767dbe570b8e887b43e0409e8e3561a43bea
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-04-05 23:50:58 +0300

    dz13 2

--- DZ11/src/services/auth.py
+++ DZ11/src/services/auth.py
@@ -9,12 +9,13 @@ from jose import JWTError, jwt
 
 from src.database.db import get_db
 from src.repository import users as repository_users
+from src.conf.config import config
 
 
 class Auth:
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-    SECRET_KEY = "b916edc3f7c5ab5339271a42fb0f83e9c9a600a53c8cefedff492bbfd5c8993d"  # TODO прибрать в ENV файл
-    ALGORITHM = "HS256"
+    SECRET_KEY = config.SECRET_KEY_JWT
+    ALGORITHM = config.ALGORITHM
 
     def verify_password(self, plain_password, hashed_password):
         return self.pwd_context.verify(plain_password, hashed_password)
