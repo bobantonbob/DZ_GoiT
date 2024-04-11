710ca551042a8f8030cd9c8a3db175e40977c6f3
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-04-11 23:46:29 +0300

    dz14 pytest_1/2

--- DZ11/tests/test_e2e_contacts.py
+++ DZ11/tests/test_e2e_contacts.py
@@ -5,28 +5,39 @@ import pytest
 from src.services.auth import auth_service
 
 
-def test_get_todos(client, get_token):
+def test_get_contacts(client, get_token):
     with patch.object(auth_service, 'cache') as redis_mock:
         redis_mock.get.return_value = None
         token = get_token
         headers = {"Authorization": f"Bearer {token}"}
-        response = client.get("api/todos", headers=headers)
+        response = client.get("api/contacts", headers=headers)
         assert response.status_code == 200, response.text
         data = response.json()
         assert len(data) == 0
 
 
-def test_create_todo(client, get_token, monkeypatch):
+def test_create_contact(client, get_token, monkeypatch):
     with patch.object(auth_service, 'cache') as redis_mock:
         redis_mock.get.return_value = None
         token = get_token
         headers = {"Authorization": f"Bearer {token}"}
-        response = client.post("api/todos", headers=headers, json={
-            "title": "test",
-            "description": "test",
+        response = client.post("api/contacts", headers=headers, json={
+            "first_name": "test",
+            "last_name": "test",
+            "email": "test@test.com",
+            "phone_number": "test",
+            "birthday": "test",
+            "extra_info": "test",
+            # "completed": "test",
         })
         assert response.status_code == 201, response.text
         data = response.json()
         assert "id" in data
-        assert data["title"] == "test"
-        assert data["description"] == "test"
+        assert data["first_name"] == "test"
+        assert data["last_name"] == "test"
+        assert data["email"] == "test@test.com"
+        assert data["phone_number"] == "test"
+        assert data["birthday"] == "test"
+        assert data["extra_info"] == "test"
+        # assert data["completed"] == "test"
+
